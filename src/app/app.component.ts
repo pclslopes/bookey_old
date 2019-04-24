1c904b9f9972dfde4a11fbc8ea10a4688493badb
pclslopes <pclslopes@gmail.com>
2019-04-24 23:04:46 +0100

    2 files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, Optional } from '@angular/core';
+import { Component, Optional, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
-
+import { Validators } from "@angular/forms";
+import { FieldConfig } from "../../dynamic-forms/interfaces/dynamic-field.interface";
+import { DynamicFormComponent } from "../../dynamic-forms/componentsdynamic-form/dynamic-form.component";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,96 @@ import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  
-  constructor() {
-    
-  }
+   @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+  regConfig: FieldConfig[] = [
+    {
+      type: "input",
+      label: "Username",
+      inputType: "text",
+      name: "name",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Name Required"
+        },
+        {
+          name: "pattern",
+          validator: Validators.pattern("^[a-zA-Z]+$"),
+          message: "Accept only text"
+        }
+      ]
+    },
+    {
+      type: "input",
+      label: "Email Address",
+      inputType: "email",
+      name: "email",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Email Required"
+        },
+        {
+          name: "pattern",
+          validator: Validators.pattern(
+            "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"
+          ),
+          message: "Invalid email"
+        }
+      ]
+    },
+    {
+      type: "input",
+      label: "Password",
+      inputType: "password",
+      name: "password",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Password Required"
+        }
+      ]
+    },
+    {
+      type: "radiobutton",
+      label: "Gender",
+      name: "gender",
+      options: ["Male", "Female"],
+      value: "Male"
+    },
+    {
+      type: "date",
+      label: "DOB",
+      name: "dob",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Date of Birth Required"
+        }
+      ]
+    },
+    {
+      type: "select",
+      label: "Country",
+      name: "country",
+      value: "UK",
+      options: ["India", "UAE", "UK", "US"]
+    },
+    {
+      type: "checkbox",
+      label: "Accept Terms",
+      name: "term",
+      value: true
+    },
+    {
+      type: "button",
+      label: "Save"
+    }
+  ];
+
+  submit(value: any) {}
 }
---
+++ src/app/components/form-template/form-template.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild } from "@angular/core";
+import { Validators } from "@angular/forms";
+import { FieldConfig } from "../../dynamic-forms/interfaces/dynamic-field.interface";
+import { DynamicFormComponent } from "../../dynamic-forms/components/dynamic-form/dynamic-form.component";
+
+@Component({
+  selector: 'app-form-template',
+  templateUrl: './form-template.component.html',
+  styleUrls: ['./form-template.component.css']
+})
+export class FormTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
