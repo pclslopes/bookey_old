fd5120e8bb80159cd00f21bef8e799f502b09242
pclslopes <pclslopes@gmail.com>
2019-04-24 22:43:43 +0100

    created component folder

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from '../dashboard/dashboard.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
