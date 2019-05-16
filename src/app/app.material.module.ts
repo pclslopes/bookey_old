5c839cf690c31fd1f3a3029055b4291a324cf988
pclslopes <pclslopes@gmail.com>
2019-05-16 10:41:45 +0100

    4 files modified

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatMomentDateModule } from "@angular/material-moment-adapter";
 import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatChipsModule, MatOptionModule, MatGridListModule, MatProgressBarModule, MatSliderModule, MatSlideToggleModule, MatMenuModule, MatDialogModule, MatSnackBarModule, MatSelectModule, MatInputModule, MatSidenavModule, MatCardModule, MatIconModule, MatRadioModule, MatProgressSpinnerModule, MatTabsModule, MatListModule, MatNativeDateModule, MatDatepickerModule } from '@angular/material';
 
+
 @NgModule({
   imports: [
     MatButtonModule, MatCheckboxModule, MatToolbarModule, MatChipsModule, MatOptionModule, MatGridListModule, MatProgressBarModule, MatSliderModule, MatSlideToggleModule, MatMenuModule, MatDialogModule, MatSnackBarModule, MatSelectModule, MatInputModule, MatSidenavModule, MatCardModule, MatIconModule, MatRadioModule, MatProgressSpinnerModule, MatTabsModule,MatListModule, MatMomentDateModule, MatNativeDateModule, MatDatepickerModule
