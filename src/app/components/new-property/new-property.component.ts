d4a88fc8379db2fe847b22c3656c153905eabfc3
pclslopes <pclslopes@gmail.com>
2019-05-22 10:11:11 +0100

    Properties

--- src/app/components/new-property/new-property.component.ts
+++ src/app/components/new-property/new-property.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Location } from '@angular/common';
+import { Validators } from "@angular/forms";
+import { DynamicFormComponent } from "../../dynamic-forms/components/dynamic-form/dynamic-form.component";
+import { FieldConfig } from "../../dynamic-forms/interfaces/dynamic-field.interface";
+import { MatSnackBar } from '@angular/material';
+import { Router, RouterModule, Params } from '@angular/router';
+import { AuthParseService } from '../../services/auth.parse.service'
 
 @Component({
   selector: 'app-new-property',
@@ -7,9 +14,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPropertyComponent implements OnInit {
 
-  constructor() { }
+  isEdit = false;
+
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+    regConfig: FieldConfig[] = [
+    {
+      type: "input",
+      label: "Property Name",
+      inputType: "text",
+      name: "propertyName",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Property Name Required"
+        },
+        {
+          name: "pattern",
+          validator: Validators.pattern("^[a-zA-Z ]+$"),
+          message: "Accept only text"
+        }
+      ]
+    },
+    {
+      type: "input",
+      label: "Link",
+      inputType: "text",
+      name: "link",
+      validations: [
+        {
+          name: "pattern",
+          validator: Validators.pattern("^[a-zA-Z ]+$"),
+          message: "Accept only text"
+        }
+      ]
+    }
+  ];
+  constructor(
+    public authService: AuthParseService,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private location: Location) { }
 
   ngOnInit() {
   }
 
+  onSubmit(value: any) {
+      
+      if(this.form.form.valid){
+        //const formValue = this.questionsForm.value;
+        alert(JSON.stringify(value));
+      }else{
+        //form.form.validations
+        this.snackbar.open('Please fill all mandatory fields', 'OK', { duration: 3000 });
+        return;
+      }
+  }
+  
+  onCancel(){
+    this.location.back();
+  }
 }
