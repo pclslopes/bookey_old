cfc9b5a4a846ad605def2af135a893eb41eeaeff
pclslopes <pclslopes@gmail.com>
2019-05-22 17:48:19 +0100

    1 file modified

--- src/app/components/properties/properties.component.ts
+++ src/app/components/properties/properties.component.ts
@@ -11,7 +11,7 @@ import { PropertyModel } from '../../models/property.model';
 })
 export class PropertiesComponent implements OnInit {
 
-  properties: PropertyModel[];
+  properties: any;
   displayedColumns: string[] = [ 'PropertyName', 'PropertyLink'];
 
   constructor(public authService: AuthParseService,
@@ -20,60 +20,34 @@ export class PropertiesComponent implements OnInit {
       public router: Router) { }
 
   ngOnInit() {
-  //  this.propertyService.getBookings().subscribe(data => {
-   //   this.properties = data.map(e => {
-   //     return {
-   //       objectId: e.payload.doc.id,
-   //       PropertyName:  e.payload.doc.data().PropertyName,
-   //       PropertyLink:  e.payload.doc.data().PropertyLink
-   //     } as PropertyModel;
-  //    })
-  //  });
-
-//    .map(res => { 
-//        return res.json().results.map(item => { 
- //         return new SearchItem( (4)
- //             item.trackName,
-  ////////            item.artistName,
-//              item.trackViewUrl,
- //             item.artworkUrl30,
-  //            item.artistId
-   //       );
-    //    });
-     // });
-      //alert(JSON.stringify());
-
-     
-
-  this.propertyService.getProperties2().then((res) => {
+ 
+    this.propertyService.getProperties2().then((res) => {
       console.log("replied");
-      console.log("-->"+JSON.stringify(res)); // not called
-      //this.properties = res.map(item => new PropertyModel(item));
+      console.log("res: -->"+JSON.stringify(res)); // not called
+      this.properties = res.map(item => {
+        return {
+          objectId: [...item].objectId,
+          PropertyName: item.PropertyName,
+          PropertyLink: item.PropertyLink
+        } as PropertyModel
+      });
       //Object.assign(new PropertyModel(), res)
       for (var c in res) {
         console.log(JSON.stringify(res[c]));
       }
+      console.log("count: " + res.count);
+      var arr_names:PropertyModel[] = new Array(res._count);
       Array.from(Object.keys(res)).forEach(function(key) {
+        //this.arr_names.push(res[key]);
           console.log(key + ':--> ' + JSON.stringify(res[key]));
           console.log(key + ':--> ' + JSON.stringify(res[key].PropertyName));
 
           
       });
       
-       this.properties = Object.keys(res).map((item, index) => {
-          const model = new PropertyModel();
-          model.PropertyName = res[index].PropertyName
-          //return Object.assign(model, res[index]);
-          //return model;
-       });
       
-      
-//      this.properties = Object.assign(new PropertyModel(), res);
-      //console.log("final obj: "+JSON.stringify(Object.assign(new PropertyModel(), res)));
+
       console.log("final obj2: "+JSON.stringify(this.properties));
-//    })
-//    .catch((err) => {
-//        console.log(err.message);
     });
   }
 
