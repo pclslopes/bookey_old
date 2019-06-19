cd93d51f1db3599b47f788b0395fb2b08a293ef6
pclslopes <pclslopes@gmail.com>
2019-06-19 16:16:28 +0100

    Dynamic AutoComplete

---
+++ src/app/dynamic-forms/components/dynamic-autocomplete/dynamic-autocomplete.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { FieldConfig } from "../../interfaces/dynamic-field.interface";
+@Component({
+  selector: "app-autocomplete",
+  template: `
+<mat-form-field class="demo-full-width margin-top" [formGroup]="group"  appearance="outline" floatLabel="always" hideRequiredMarker="false">
+<mat-label>{{field.label}}</mat-label>
+<mat-autocomplete [placeholder]="field.label" [formControlName]="field.name">
+<mat-option *ngFor="let item of field.options" [value]="item.id">{{item.name}}</mat-option>
+</mat-autocomplete>
+</mat-form-field>
+`,
+  styles: []
+})
+export class DynamicSelectComponent implements OnInit {
+  field: FieldConfig;
+  group: FormGroup;
+  constructor() {}
+  ngOnInit() {}
+}
