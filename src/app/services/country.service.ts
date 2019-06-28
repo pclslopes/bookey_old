05104d8d9f3c01a8c42947b8dc6f1a0ba0ed4941
pclslopes <pclslopes@gmail.com>
2019-06-28 10:50:49 +0100

    Country Service

---
+++ src/app/services/country.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { CustomerModel } from '../models/customer.model';
+import { AuthParseService } from '../services/auth.parse.service';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+declare const Parse: any;
+
+@Injectable()
+export class CountryService {
+
+  constructor(
+      public authService: AuthParseService,
+    ) { 
+    Parse.initialize(environment.parseServer.PARSE_APP_ID, environment.parseServer.PARSE_JS_KEY);
+    Parse.serverURL = environment.parseServer.serverURL;
+  }
+
+  public getAllCountries(){
+    return new Promise((resolve, reject) => {
+      // Setup Parse
+      var parseObj = Parse.Object.extend("Countries");
+      var query = new Parse.Query(parseObj);
+      // Query
+      query.descending('name');
+      query.find().then((results) => {
+        resolve(results.map(r => ({
+          id: r.id,
+          name: r.get("name")
+        })))
+      },(error) => {
+        reject(error);
+      });
+    });
+  }
+}
