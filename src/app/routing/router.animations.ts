8e3222d847765d5b89ec385e2845e5e06ee01759
pclslopes <pclslopes@gmail.com>
2019-07-02 17:30:06 +0100

    1 file modified

--- src/app/routing/router.animations.ts
+++ src/app/routing/router.animations.ts
@@ -1,38 +1,34 @@
-import {trigger, state, animate, style, transition} from '@angular/core';
+import {
+  trigger, animateChild, group,
+  transition, animate, style, query
+} from '@angular/animations';
 
-export function moveIn() {
-  return trigger('moveIn', [
-    state('void', style({position: 'fixed', width: '100%'}) ),
-    state('*', style({position: 'fixed', width: '100%'}) ),
-    transition(':enter', [
-      style({opacity:'0', transform: 'translateX(100px)'}),
-      animate('.6s ease-in-out', style({opacity:'1', transform: 'translateX(0)'}))
-    ]),
-    transition(':leave', [
-      style({opacity:'1', transform: 'translateX(0)'}),
-      animate('.3s ease-in-out', style({opacity:'0', transform: 'translateX(-200px)'}))
+
+// Routable animations
+export const slideInAnimation =
+  trigger('routeAnimation', [
+    transition('heroes <=> hero', [
+      style({ position: 'relative' }),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ]),
+      query(':enter', [
+        style({ left: '-100%'})
+      ]),
+      query(':leave', animateChild()),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', style({ left: '100%'}))
+        ]),
+        query(':enter', [
+          animate('300ms ease-out', style({ left: '0%'}))
+        ])
+      ]),
+      query(':enter', animateChild()),
     ])
   ]);
-}
-
-export function fallIn() {
-  return trigger('fallIn', [
-    transition(':enter', [
-      style({opacity:'0', transform: 'translateY(40px)'}),
-      animate('.4s .2s ease-in-out', style({opacity:'1', transform: 'translateY(0)'}))
-    ]),
-    transition(':leave', [
-      style({opacity:'1', transform: 'translateX(0)'}),
-      animate('.3s ease-in-out', style({opacity:'0', transform: 'translateX(-200px)'}))
-    ])
-  ]);
-}
-
-export function moveInLeft() {
-  return trigger('moveInLeft', [
-    transition(':enter', [
-      style({opacity:'0', transform: 'translateX(-100px)'}),
-      animate('.6s .2s ease-in-out', style({opacity:'1', transform: 'translateX(0)'}))
-    ])
-  ]);
-}
